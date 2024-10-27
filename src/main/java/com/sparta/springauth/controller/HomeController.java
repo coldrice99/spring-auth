ecb8e593b81e670e9537eae5cde49a0c50c46945
coldrice99 <coldrice99@gmail.com>
2024-10-27 18:41:54 +0900

    spring security : JWT 로그인
    - 이제 컨트롤러 쪽에는 인증/인가 코드x, 필터에서 전부 처리

--- src/main/java/com/sparta/springauth/controller/HomeController.java
+++ src/main/java/com/sparta/springauth/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.security.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    /* @AuthenticationPrincipal 를 사용하여 username을 동적으로 넣어줌
+       @AuthenticationPrincipal UserDetailsImpl userDetails: Spring Security에서 인증된 사용자 정보를 얻기 위해 사용
+     */
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("username", "username");
+    public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        model.addAttribute("username", userDetails.getUsername());
         return "index";
     }
 }
--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.sparta.springauth.controller;
 
-import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,16 +32,5 @@ public class UserController {
         userService.signup(requestDto);
 
         return "redirect:/api/user/login-page"; // 로그인 페이지가 템플릿에서 호출될 것임
-    }
-
-    @PostMapping("/user/login")
-    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
-        try {
-            userService.login(requestDto, res);
-        } catch (Exception e) {
-            return "redirect:/api/user/login-page?error";
-        }
-
-        return "redirect:/"; // 메인으로 리다이렉트
     }
 }
