e32150fb6fb9650817dd5dd4ba1093514f1f8ef6
coldrice99 <coldrice99@gmail.com>
2024-10-27 22:05:28 +0900

    validation 예외처리

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -2,11 +2,18 @@ package com.sparta.springauth.controller;
 
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
@@ -28,9 +35,18 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto) {
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";  // 다시 회원가입하라는 뜻으로 리다이렉트
+        }
+
         userService.signup(requestDto);
 
-        return "redirect:/api/user/login-page"; // 로그인 페이지가 템플릿에서 호출될 것임
+        return "redirect:/api/user/login-page"; // 회원가입 후 로그인 페이지로
     }
 }
